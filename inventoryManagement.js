0c875a457e69b45a07c6ce6d4e1552c07f4e1bfc
wanjirumuthike18 <wanjirumuthike18@gmail.com>
2026-06-20 16:14:03 +0300

    Complete inventory management lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,4 +1,20 @@
-// Write your code here
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+function addProduct(productName) {
+  products.push(productName);
+}
+
+function updateProductName(position, newName) {
+  products[position] = newName;
+}
+
+function removeLastProduct() {
+  products.pop();
+}// Write your code here
 
 
 
